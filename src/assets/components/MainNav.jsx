78f336446b69a566c13bdbad8b7cd9b7b4bee889
Alessandro-Frisone <alefri2004@gmail.com>
2025-02-08 11:58:59 +0100

    feat: add hover on main nav

--- src/assets/components/MainNav.jsx
+++ src/assets/components/MainNav.jsx
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 export default function MainNav() {
   return (
     <nav className="flex justify-center items-center align-center">
-      <img className="w-28 h-28" src="./public/img/LOGO_138.png" alt="" />
+      <img className="w-28 h-28" src="./img/LOGO_138.png" alt="" />
       <ul className="flex justify-center gap-12 font-extrabold text-xl p-8 w-full">
-        <li>
+        <li className="cursor-pointer transition-transform duration-200 hover:scale-115">
           <NavLink to="/">Home Page</NavLink>
         </li>
-        <li>
+        <li className="cursor-pointer transition-transform duration-200 hover:scale-115">
           <NavLink to="/ContactUs">Contact Us</NavLink>
         </li>
-        <li>
+        <li className="cursor-pointer transition-transform duration-200 hover:scale-115">
           <NavLink to="/Posts">Posts</NavLink>
         </li>
       </ul>
