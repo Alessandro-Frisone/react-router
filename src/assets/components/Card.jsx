a498c689ec196267cf07456e634d2009139c4f43
Alessandro-Frisone <alefri2004@gmail.com>
2025-02-10 21:38:09 +0100

    fix: title color

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({ post }) {
   return (
     <Link to={`/Posts/${post.id}`}>
       <div className="mt-[35px] mb-[35px] border border-gray-300 px-4 py-2 text-lg rounded-lg shadow-lg transition-transform duration-500 hover:scale-105 cursor-pointer">
-        <p className="text-center text-3xl">{post.title}</p>
+        <p className="text-center text-3xl text-[#d4bb9b]">{post.title}</p>
         <p className="text-center mt-[35px] ">{post.content}</p>
         <img
           className="mx-auto mt-4 w-130 h-80"
